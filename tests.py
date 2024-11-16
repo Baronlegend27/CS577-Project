52af06901728cc0ff388d927027ebbcc395d2e39
Vishvam Patel <114180674+n0varider@users.noreply.github.com>
2024-11-16 16:59:03 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 df1 = pd.read_csv('data/anime.csv')
 df2 = pd.read_csv('data/rating.csv')
-df2 = df2[:100000]
+df2 = df2[:300000]
 
 
 df1.dropna(inplace=True)
@@ -21,8 +21,8 @@ handler = AnimeData(df2, df1)
 user_count = len(handler.ratings['user_id'].unique())  # Number of unique users
 item_count = len(handler.anime['anime_id'].unique())  # Number of unique anime
 genre_count = 43  # Number of one-hot encoded genre categories
-latent_dim_len = 8  # Latent dimension length for embeddings
-hidden_layers = [64, 16, 4]  # Example hidden layers for MLP
+latent_dim_len = 20  # Latent dimension length for embeddings
+hidden_layers = [128, 64, 32, 16]  # Example hidden layers for MLP
 
 # Initialize the MLP model
 model = MLPModel(user_count=user_count,
@@ -30,11 +30,15 @@ model = MLPModel(user_count=user_count,
                  genre_count=genre_count,
                  latent_dim_len=latent_dim_len,
                  hidden=hidden_layers)
+model.load_state_dict(torch.load("models/anime.pth"))
 model.to(device)
 a, b = handler.get_loaders()
 
 loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+
+newuser = [2948, 18671, "TV", "Chuunibyou demo Koi ga Shitai! Ren", 12, "Comedy, Drama, Romance, School, Slice of Life"]
 
 
 for i in range(10):
@@ -60,7 +64,8 @@ for i in range(10):
 
         total_loss += loss.item()
     print(f"Epoch [{i + 1}/{10}], Loss: {total_loss / len(a)}")
-#torch.save(model.state_dict(), f'models/anime.pth')
+torch.save(model.state_dict(), f'models/anime.pth')
+
 model.eval()  # Set the model to evaluation mode
 total_loss = 0
 total_samples = 0
@@ -78,7 +83,7 @@ with torch.no_grad():  # Disable gradient calculation
         # Calculate the loss (e.g., MSE loss)
         loss = loss_function(predictions, labels)
         total_loss += loss.item() * len(labels)  # Accumulate loss
-        total_samples += len(targets)  # Accumulate the number of samples
+        total_samples += len(labels)  # Accumulate the number of samples
 
 # Calculate average loss
 avg_loss = total_loss / total_samples
